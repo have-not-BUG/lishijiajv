b29ccf4ab87a24a4facfcfec2e1d116114f6827c
have-not-BUG <lxyxdylglc@qq.com>
2017-07-24 08:15:07 +0800

    增加leanclound支持

--- data/mongoose.js
+++ data/mongoose.js
@@ -3,10 +3,10 @@ var mongoose = require('mongoose');
 // mongoose.connect('mongodb://localhost/mobile');
 mongoose.connect('mongodb://mgusername:mgusername@ds149501.mlab.com:49501/mobile');
 var db = mongoose.connection;
-db.on('error', console.error.bind(console, '数据库mongoose连接错误啊！:'));
-db.once('open', function() {
-    console.log("数据库mongoose连接成功");
-});
+// db.on('error', console.error.bind(console, '数据库mongoose连接错误啊！:'));
+// db.once('open', function() {
+//     console.log("数据库mongoose连接成功");
+// });
 
 var blogSchema = new mongoose.Schema({
     name: String,
